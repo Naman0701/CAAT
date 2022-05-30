6bbb65eddb7a4c2480b97515db82c8ea9133a619
Naman <nnaman33@gmail.com>
2022-05-31 02:35:40 +0530

    Heroku_9

--- CAAT/settings.py
+++ CAAT/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET']
 
 
-ALLOWED_HOSTS = ['caat0701.herokuapp.com','localhost']
+ALLOWED_HOSTS = ['caat0701.herokuapp.com','localhost','college-erp-0701.herokuapp.com']
 
 DEBUG=False
 
